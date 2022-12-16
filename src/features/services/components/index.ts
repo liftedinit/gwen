aa6e53c17d9c6c3fa2d777f0ca6c87481958a721
Maximiliano José Sorribas <mjsorribas@gmail.com>
2022-12-16 15:41:31 -0300

    feat:update service index file, remove duplicated export

--- src/features/services/components/index.ts
+++ src/features/services/components/index.ts
@@ -1,7 +1,6 @@
 export * from "./service";
 export * from "./ledger-settings";
 export * from "./blocks-settings";
-export * from "./data-settings";
 export * from "./create-token-modal";
 export * from "./kvdata-settings";
 export * from "./create-kvdata-modal";
