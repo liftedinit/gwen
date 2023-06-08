8bbff1515028e072722f488f93dd338fa8ec5add
Stanley Jones <stanley@sunshocked.com>
2023-06-08 13:22:54 -0700

    fixup: Forgot some files

---
+++ src/api/services/queries/data.ts
@@ -0,0 +1,133 @@
+import { Network } from "@liftedinit/many-js";
+import { useMutation, useQueries, useQuery, useQueryClient } from "react-query";
+
+export function useListKeys(neighborhood: Network | undefined) {
+  return useQuery(
+    [neighborhood?.url, "kvStore.list"],
+    async () => await neighborhood?.kvStore.list(),
+    { enabled: !!neighborhood }
+  );
+}
+
+export function useCombinedData(neighborhood: Network | undefined) {
+  const keys = useListKeys(neighborhood);
+  const values = useGetValues(neighborhood, keys.data?.keys);
+  const queries = useQueryValues(neighborhood, keys.data?.keys);
+
+  const combined = [keys, ...values, ...queries];
+
+  const all = new Map<string, any>();
+
+  if (values.every((q) => q.isSuccess)) {
+    values
+      .map((q) => q.data)
+      .forEach((d) => all.set(d.key, { ...all.get(d.key), ...d }));
+  }
+
+  if (queries.every((q) => q.isSuccess)) {
+    queries
+      .map((q) => q.data)
+      .forEach((d) => all.set(d.key, { ...all.get(d.key), ...d }));
+  }
+
+  return {
+    data: Object.fromEntries(all),
+    isLoading: combined.some((q) => q.isLoading),
+    isSuccess: combined.every((q) => q.isSuccess),
+    isError: combined.some((q) => q.isError),
+    errors: combined.map((q) => q.error),
+  };
+}
+
+export function usePutValue(neighborhood: Network | undefined) {
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    async ({ key, value }: { key: string; value: string }) =>
+      await neighborhood?.kvStore.put({ key, value }),
+    {
+      onSuccess: (_, key) => {
+        queryClient.invalidateQueries({
+          queryKey: [neighborhood?.url, "kvStore.get", key],
+        });
+      },
+    }
+  );
+}
+
+function getValue(neighborhood: Network | undefined, key: string) {
+  return {
+    queryKey: [neighborhood?.url, "kvStore.get", key],
+    queryFn: async () => await neighborhood?.kvStore.get({ key }),
+    enabled: !!neighborhood,
+  };
+}
+
+export function useGetValue(neighborhood: Network | undefined, key: string) {
+  return useQuery(getValue(neighborhood, key));
+}
+
+export function useGetValues(
+  neighborhood: Network | undefined,
+  keys: string[] = []
+) {
+  return useQueries({
+    queries: keys?.map((key) => getValue(neighborhood, key)),
+  });
+}
+
+function queryValue(neighborhood: Network | undefined, key: string) {
+  return {
+    queryKey: [neighborhood?.url, "kvStore.query", key],
+    queryFn: async () => await neighborhood?.kvStore.query({ key }),
+    enabled: !!neighborhood,
+  };
+}
+
+export function useQueryValue(neighborhood: Network | undefined, key: string) {
+  return useQuery(queryValue(neighborhood, key));
+}
+
+export function useQueryValues(
+  neighborhood: Network | undefined,
+  keys: string[] = []
+) {
+  return useQueries({
+    queries: keys?.map((key) => queryValue(neighborhood, key)),
+  });
+}
+
+export function useMarkImmutable(neighborhood: Network | undefined) {
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    async (key: string) =>
+      await neighborhood?.kvStore.transfer({ key, newOwner: "maiyg" }),
+    {
+      onSuccess: (_, key) => {
+        queryClient.invalidateQueries([
+          neighborhood?.url,
+          "kvStore.query",
+          key,
+        ]);
+      },
+    }
+  );
+}
+
+export function useDisableKey(neighborhood: Network | undefined) {
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    async (key: string) => await neighborhood?.kvStore.disable({ key }),
+    {
+      onSuccess: (_, key) => {
+        queryClient.invalidateQueries([
+          neighborhood?.url,
+          "kvStore.query",
+          key,
+        ]);
+      },
+    }
+  );
+}
---
+++ src/pages/services/data/put-value-modal.tsx
@@ -0,0 +1,117 @@
+import {
+  Alert,
+  AlertIcon,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Grid,
+  GridItem,
+  Input,
+  Modal,
+  Textarea,
+  useToast,
+} from "@liftedinit/ui";
+import { NeighborhoodContext } from "api/neighborhoods";
+import { usePutValue } from "api/services";
+import { useContext } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+
+interface PutValueInputs {
+  key: string;
+  value: string;
+}
+
+export function PutValueModal({
+  isOpen,
+  onClose,
+  itemKey,
+  itemValue,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  itemKey: string;
+  itemValue: string;
+}) {
+  const neighborhood = useContext(NeighborhoodContext);
+  const {
+    mutate: doPutValue,
+    error,
+    isError,
+    isLoading,
+  } = usePutValue(neighborhood);
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<PutValueInputs>();
+  const toast = useToast();
+
+  const onSubmit: SubmitHandler<PutValueInputs> = ({ key, value }) => {
+    doPutValue(
+      { key, value },
+      {
+        onSuccess: () => {
+          onClose();
+          toast({
+            status: "success",
+            title: "Put",
+            description: "Data was written",
+          });
+        },
+      }
+    );
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal.Header>New Entry</Modal.Header>
+      <Modal.Body>
+        <Grid templateColumns="repeat(5,1fr)" gap={9}>
+          <GridItem colSpan={5}>
+            <FormControl isInvalid={!!errors.key}>
+              <FormLabel htmlFor="key">Key</FormLabel>
+              <Controller
+                name="key"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => <Input {...field} />}
+                defaultValue={itemKey}
+              />
+            </FormControl>
+          </GridItem>
+          <GridItem colSpan={5}>
+            <FormControl isInvalid={!!errors.value}>
+              <FormLabel htmlFor="value">Value</FormLabel>
+              <Controller
+                name="value"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => <Textarea {...field} />}
+                defaultValue={itemValue}
+              />
+            </FormControl>
+          </GridItem>
+        </Grid>
+      </Modal.Body>
+      <Modal.Footer>
+        <Flex justifyContent="flex-end" w="full">
+          <Button
+            isLoading={isLoading}
+            onClick={handleSubmit(onSubmit)}
+            width={{ base: "full", md: "auto" }}
+            colorScheme="brand.teal"
+          >
+            Save
+          </Button>
+        </Flex>
+        {isError && (
+          <Alert status="error">
+            <AlertIcon />
+            {(error as Error).message}
+          </Alert>
+        )}
+      </Modal.Footer>
+    </Modal>
+  );
+}
