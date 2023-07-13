6a4345f5c09598056a55e996b85c51c209c98f59
Stanley Jones <stanley@sunshocked.com>
2023-07-13 13:12:45 -0700

    build: Upgrade to Node v18.16.1

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,24 @@
+const webpack = require("webpack");
+
 module.exports = function override(webpackConfig) {
+  webpackConfig.resolve.fallback = {
+    buffer: require.resolve("buffer"),
+    crypto: require.resolve("crypto-browserify"),
+    process: require.resolve("process/browser"),
+    stream: require.resolve("stream-browserify"),
+  };
+  webpackConfig.plugins.push(
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    })
+  );
   webpackConfig.module.rules.push({
     test: /\.mjs$/,
     include: /node_modules/,
     type: "javascript/auto",
+    resolve: {
+      fullySpecified: false,
+    },
   });
 
   return webpackConfig;
